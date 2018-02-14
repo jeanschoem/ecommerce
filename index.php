3aefd708b792d28ee52a7b44a544a9057a82cc12
Jean <jeanschoe@gmail.com.br>
2018-02-13 23:05:21 -0200

    Tela de Login

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php 
-
+session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
-use \Hcode\Page; 
+use \Hcode\Page;
+use \Hcode\PageAdmin; 
+use \Hcode\Model\User;
 
 $app = new Slim();
 
@@ -14,6 +16,44 @@ $app->get('/', function() {
     $page = new Page();
     $page-> setTpl("index");
 	
+});
+
+$app->get('/admin', function() {
+    User::verifyLogin();
+    $page = new PageAdmin();
+    $page-> setTpl("index");
+	
+});
+
+$app->get('/admin/login', function() {
+    if (!isset($_SESSION[User::SESSION]))
+    {
+	    $page = new PageAdmin([
+	    	"header"=> false,
+	    	"footer"=> false
+
+	    ]);
+	    $page-> setTpl("login");    	
+    }else{
+    	header("Location: /admin");
+    	exit;
+    }
+
+});
+
+$app->post('/admin/login', function() {
+    
+    User::login($_POST["login"], $_POST["password"]);
+    header("Location: /admin");
+    exit;
+});
+
+$app->get('/admin/logout', function() {
+    
+    User::logout();
+    header("Location: /admin/login");
+    exit;
+
 });
 
 $app->run();
